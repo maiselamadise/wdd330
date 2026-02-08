63687d4ac7fe0f9942ecd5bb0372857923d87d54
maiselamadise <maisela.mad@gmail.com>
2026-02-09 01:37:38 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "src/",
 
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 5173,
+  },
+
   build: {
     outDir: "../dist",
     rollupOptions: {
@@ -13,16 +18,19 @@ export default defineConfig({
         checkout: resolve(__dirname, "src/checkout/index.html"),
         product1: resolve(
           __dirname,
-          "src/product_pages/cedar-ridge-rimrock-2.html",
+          "src/product_pages/cedar-ridge-rimrock-2.html"
         ),
-        product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
+        product2: resolve(
+          __dirname,
+          "src/product_pages/marmot-ajax-3.html"
+        ),
         product3: resolve(
           __dirname,
-          "src/product_pages/northface-alpine-3.html",
+          "src/product_pages/northface-alpine-3.html"
         ),
         product4: resolve(
           __dirname,
-          "src/product_pages/northface-talus-4.html",
+          "src/product_pages/northface-talus-4.html"
         ),
       },
     },
