e03b9c566e5e3936ff24df2d84d8df013e327415
maiselamadise <maisela.mad@gmail.com>
2026-02-16 00:31:00 +0200

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, alertMessage } from "./utils.js";
 
-loadHeaderFooter();
+document.addEventListener("DOMContentLoaded", init);
+
+async function init() {
+  try {
+    // Load shared header and footer
+    await loadHeaderFooter();
+  } catch (error) {
+    console.error("Initialization error:", error);
+    alertMessage("Something went wrong loading the page.");
+  }
+}
